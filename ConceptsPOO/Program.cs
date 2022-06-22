230e338b0f0636f0cec2eb5927ee438c8a7e0c03
Jonathan Carrillo <jhonatancarrillo78@gmail.com>
2022-06-22 13:51:30 -0400

    Finish date class

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                Console.WriteLine(new Date(2022, 12, 7));
+                Console.WriteLine(new Date(1974, 9, 23));
+                Console.WriteLine(new Date(2022, 11, 30));
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+        }
+    }
+}
