5ce365769e5bcd9414feb7669a473867906beaa1
Jonathan Carrillo <jhonatancarrillo78@gmail.com>
2022-06-23 15:57:07 -0400

    Employee, SalaryEmployee & CommissionEmployee finish

---
+++ ConceptsPOO/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceptsPOO
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string  FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDate { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+        public abstract decimal GetValueToPay();
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}," +
+                $"Birth: {BirthDate}," + 
+                $"Hiring: {HiringDate}," +
+                $"Is active: {IsActive}";
+        }
+    }
+}
--- ConceptsPOO/Program.cs
+++ ConceptsPOO/Program.cs
@@ -8,17 +8,41 @@ namespace ConceptsPOO
         {
             Console.WriteLine("Hello World!");
 
-            try
+            Employee employee1 = new SalaryEmployee()
             {
-                Console.WriteLine(new Date(2022, 12, 7));
-                Console.WriteLine(new Date(1974, 9, 23));
-                Console.WriteLine(new Date(2022, 11, 30));
-            }
-            catch ( Exception ex)
+                Id = 1010,
+                FirstName = "sandra",
+                LastName = "morales",
+                BirthDate = new Date(1990, 5, 23),
+                HiringDate = new Date(2022, 1, 15),
+                IsActive = true,
+                Salary = 1815453.45M
+            };
+            Console.WriteLine(employee1);
+            Employee employee2 = new CommissionEmployee()
             {
-                Console.WriteLine(ex.Message);
-                
-            }
+                Id = 2020,
+                FirstName = "Patricia",
+                LastName = "Gutierrez",
+                BirthDate = new Date(1995, 5, 23),
+                HiringDate = new Date(2022, 2, 5),
+                IsActive = true,
+                Sales= 320000000M,
+                CommissionPercentaje= 0.03F
+            };
+            Console.WriteLine(employee2);
+
+            //try
+            //{
+            //    Console.WriteLine(new Date(2022, 12, 7));
+            //    Console.WriteLine(new Date(1974, 9, 23));
+            //    Console.WriteLine(new Date(2022, 11, 30));
+            //}
+            //catch ( Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+
+            //}
         }
     }
 }
