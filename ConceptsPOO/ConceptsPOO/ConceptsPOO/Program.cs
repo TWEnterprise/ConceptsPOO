fa4eb4d8a3ca094b9a23742c3257ca27237af8fc
jonathan carrillo <jhonatancarrillo78@gmail.com>
2022-09-20 13:53:44 -0400

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConceptsPOO;
+using System.Net.WebSockets;
+
+Console.WriteLine("Concepts POO!");
+Console.WriteLine("=============");
+
+try
+{
+
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(2021, 9, 23));
+	Console.WriteLine(new Date(2020, 11, 31));
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
