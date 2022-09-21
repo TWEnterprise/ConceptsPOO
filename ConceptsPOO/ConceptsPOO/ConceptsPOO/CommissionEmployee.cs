d21bcf671ae0b563e267d7bd3b38815a97159f4a
jonathan carrillo <jhonatancarrillo78@gmail.com>
2022-09-21 12:39:56 -0400

    Employee , SalaryEmployee & CommissionEmployee

---
+++ ConceptsPOO/ConceptsPOO/ConceptsPOO/CommissionEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class CommissionEmployee: Employee
+    {
+        public float CommissionPercentaje { get; set; }
+        public decimal Sale { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return Sale * (decimal)CommissionPercentaje;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommission percentaje:{$"{CommissionPercentaje:P2}", 18}" +
+                $"\n\tSales................:{$"{Sale:C2}", 18}" +
+                $"\n\tValue to pay.........:{$"{GetValueToPay():C2}", 18}";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/ConceptsPOO/Program.cs
@@ -6,16 +6,44 @@ using System.Net.WebSockets;
 Console.WriteLine("Concepts POO!");
 Console.WriteLine("=============");
 
-try
+Employee employee = new SalaryEmployee()
 {
+    Id= 1010,
+    FirstName = "Sandra",
+    LastName= "Morales",
+    BirthDate= new Date (1990, 5, 23),
+    HiringDate= new Date (2022, 1, 15),
+    IsActive=true,
+    Salary= 1815453.45M
+};
+Console.WriteLine(employee);
 
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(2021, 9, 23));
-	Console.WriteLine(new Date(2020, 11, 31));
-
-}
-catch (Exception ex)
+Employee employee1 = new CommissionEmployee()
 {
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    Sale=320000000M,
+    CommissionPercentaje= 0.03F
+   
+};
+Console.WriteLine(employee1);
 
-    Console.WriteLine(ex.Message);
-}
+
+
+//try
+//{
+
+//	Console.WriteLine(new Date(2024, 2, 29));
+//	Console.WriteLine(new Date(2021, 9, 23));
+//	Console.WriteLine(new Date(2020, 11, 31));
+
+//}
+//catch (Exception ex)
+//{
+
+//    Console.WriteLine(ex.Message);
+//}
